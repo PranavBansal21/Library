25224572e35d8f87efc332c07a564c01c5a6086c
parimal-10 <amrutkar.parimal@gmail.com>
2024-01-19 11:46:32 +0530

    second

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express  from "express"
 import bodyParser from "body-parser"
 
 const app = express();
-
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
 
 app.listen(port, () => {
     console.log(`The port ${port} is up and running`);
