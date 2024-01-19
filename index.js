25b912a38ebe31947d6d26f229cfc0bfa053f5f2
Pranav <pranav123bansal@gmail.com>
2024-01-19 14:49:13 +0530

    Packages

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express  from "express"
-import bodyParser from "body-parser"
+import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
@@ -7,33 +7,38 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/login", (req, res) => {
-    res.render("login.ejs");
+  res.render("login.ejs");
 });
 
 app.get("/signup", (req, res) => {
-    res.render("signup.ejs");
+  res.render("signup.ejs");
 });
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+  res.render("index.ejs");
 });
 
 app.get("/student", (req, res) => {
-    res.render("student.ejs");
+  res.render("student.ejs");
 });
 
 app.get("/admin", (req, res) => {
-    res.render("admin.ejs");
-})
+  res.render("admin.ejs");
+});
 
 app.get("/signup", (req, res) => {
-    res.render("signup.ejs");
+  res.render("signup.ejs");
+});
+
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+  res.send("Registered");
 });
 
 app.get("/login", (req, res) => {
-    res.render("login.ejs");
-})
+  res.render("login.ejs");
+});
 
 app.listen(port, () => {
-    console.log(`The port ${port} is up and running`);
+  console.log(`The port ${port} is up and running`);
 });
