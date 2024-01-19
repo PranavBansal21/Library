81cddd911d26160dde07701754d1236020c7c4d3
parimal-10 <amrutkar.parimal@gmail.com>
2024-01-19 12:44:16 +0530

    third

--- index.js
+++ index.js
@@ -6,9 +6,33 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup.ejs");
+});
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
+
+app.get("/student", (req, res) => {
+    res.render("student.ejs");
+});
+
+app.get("/admin", (req, res) => {
+    res.render("admin.ejs");
+})
+
+app.get("/signup", (req, res) => {
+    res.render("signup.ejs");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+})
 
 app.listen(port, () => {
     console.log(`The port ${port} is up and running`);
